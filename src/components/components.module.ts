043dae1a1ea79097b8de52a01fc2c0f303d4e634
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-11-20 23:31:45 -0500

    feat - updated home

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AccountLoginSocialComponent } from './account-login-social/account-login-social';
+@NgModule({
+	declarations: [AccountLoginSocialComponent],
+	imports: [],
+	exports: [AccountLoginSocialComponent]
+})
+export class ComponentsModule {}
