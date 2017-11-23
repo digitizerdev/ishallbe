f636b416e37f7cf2eed0681ce29740f1b617fe86
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-11-23 12:19:37 -0500

    feat - pre collaborate phase

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AccountLoginSocialComponent } from './account-login-social/account-login-social';
-@NgModule({
-	declarations: [AccountLoginSocialComponent],
-	imports: [],
-	exports: [AccountLoginSocialComponent]
-})
-export class ComponentsModule {}
