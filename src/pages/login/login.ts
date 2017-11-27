ea1660b769db63b57e208543874515a2b1e2264d
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-11-27 16:23:13 -0500

    feat - login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ForgotPasswordPage } from '../forgot-password/forgot-password';
+import { RegisterPage } from '../register/register';
 
 @IonicPage()
 @Component({
@@ -14,12 +10,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  login: {email?: string, password?: string} = {};  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  loginWithEmail() {
+    console.log("Login with email clicked");
+  }
+
+  goToForgotPasswordPage() {
+    console.log("Go to forgot password page clicked");
+    this.navCtrl.push(ForgotPasswordPage);
+  }
+
+  goToRegisterPage() {
+    console.log("Go to register page clicked");
+    this.navCtrl.push(RegisterPage);
+  }
+
+  loginWithFacebook() {
+    console.log("Login with Facebook clicked");
+  }
+  
+  loginWithLinkedin() {
+    console.log("Login with LinkedIn clicked");
   }
 
 }
