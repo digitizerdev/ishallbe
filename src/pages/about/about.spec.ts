99c36b3b4e8c18bbbf2c90ce3405c600e0e7ab56
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-12-07 20:57:08 -0500

    feat - about

---
+++ src/pages/about/about.spec.ts
@@ -0,0 +1,87 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule, Events, NavController, NavParams } from 'ionic-angular';
+import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../../environments/environment';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+
+import { HeaderComponent } from '../../components/header/header';
+import { LoginFormComponent } from '../../components/login-form/login-form';
+import { LoginFacebookComponent } from '../../components/login-facebook/login-facebook';
+import { TermsOfServiceComponent } from '../../components/terms-of-service/terms-of-service';
+
+import { AboutPage } from './about';
+
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import { SessionProvider } from '../../providers/session/session';
+
+import { } from 'jasmine';
+
+import {
+  FirebaseProviderMock,
+  SessionProviderMock,
+  NavMock,
+  StorageMock,
+  AngularFireDatabaseMock,
+  AngularFireAuthMock
+} from '../../../test-config/mocks-ionic';
+
+let fixture;
+let component;
+let session: SessionProvider;
+let sessionSpy;
+let firebase: FirebaseProvider;
+let firebaseSpy;
+
+describe('AboutPage', () => {
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AboutPage], 
+      imports: [
+        IonicModule.forRoot(AboutPage),
+        AngularFireModule.initializeApp(environment.firebase)                        
+      ],
+      providers: [
+        { provide: FirebaseProvider, useClass: FirebaseProviderMock },
+        { provide: SessionProvider, useClass: SessionProviderMock },
+        { provide: Storage, useClass: StorageMock },
+        { provide: NavController, useClass: NavMock },
+        { provide: NavParams, useClass: NavMock },
+        { provide: AngularFireDatabase, useClass: AngularFireDatabaseMock },
+        { provide: AngularFireAuth, useClass: AngularFireAuthMock },       
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutPage);
+    component = fixture.componentInstance;
+    session = fixture.componentRef.injector.get(SessionProvider);    
+    firebase = fixture.componentRef.injector.get(FirebaseProvider);
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+    session = null;
+    sessionSpy = null;
+    firebase = null;
+    firebaseSpy = null;
+  });
+
+  it('should be created', () => {
+    expect(component instanceof AboutPage).toBe(true);
+  });
+
+  it('should have title called About', () => {
+    expect(component.title).toBe('About');
+  });
+
+});
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the AboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-about',
@@ -15,11 +8,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  title = 'About';
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AboutPage');
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams
+  ) {
   }
 
 }
