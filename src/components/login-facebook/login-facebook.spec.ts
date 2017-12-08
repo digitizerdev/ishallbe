2a02286fe2ab4662470679ac4957c320facb0267
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-12-07 21:41:49 -0500

    feat - authentication

--- src/components/login-facebook/login-facebook.spec.ts
+++ src/components/login-facebook/login-facebook.spec.ts
@@ -1,35 +1,130 @@
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
-import { IonicModule } from 'ionic-angular';
-
-import {} from 'jasmine';
+import { IonicModule, Events, NavController, AlertController, LoadingController, NavParams } from 'ionic-angular';
+import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../../environments/environment';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
 
 import { LoginFacebookComponent } from './login-facebook';
 
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import { SessionProvider } from '../../providers/session/session';
+
+import { } from 'jasmine';
+
 import {
+    FirebaseProviderMock,
+    SessionProviderMock,
+    NavMock,
+    StorageMock,
+    AngularFireDatabaseMock,
+    AngularFireAuthMock,
+    LoadingControllerMock,
+    AlertControllerMock,
+    FacebookMock,
 } from '../../../test-config/mocks-ionic';
 
+let fixture;
+let component;
+let session: SessionProvider;
+let sessionSpy;
+let firebase: FirebaseProvider;
+let firebaseSpy;
+
 describe('LoginFacebookComponent', () => {
-  let fixture;
-  let component;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginFacebookComponent],
-      imports: [
-        IonicModule.forRoot(LoginFacebookComponent),
-      ],
-      providers: [     
-      ]
-    })
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginFacebookComponent],
+            imports: [
+                IonicModule.forRoot(LoginFacebookComponent),
+                AngularFireModule.initializeApp(environment.firebase)                
+            ],
+            providers: [
+                { provide: FirebaseProvider, useClass: FirebaseProviderMock },
+                { provide: SessionProvider, useClass: SessionProviderMock },
+                { provide: Storage, useClass: StorageMock },
+                { provide: NavController, useClass: NavMock },
+                { provide: NavParams, useClass: NavMock },
+                { provide: AngularFireDatabase, useClass: AngularFireDatabaseMock },
+                { provide: AngularFireAuth, useClass: AngularFireAuthMock },
+                { provide: Facebook, useClass: FacebookMock },
+                { provide: AlertController, useClass: AlertControllerMock },
+                { provide: LoadingController, useClass: LoadingControllerMock }                
+            ],
+            schemas: [
+                CUSTOM_ELEMENTS_SCHEMA
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginFacebookComponent);
-    component = fixture.componentInstance;
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginFacebookComponent);
+        component = fixture.componentInstance;
+        session = fixture.componentRef.injector.get(SessionProvider);
+        firebase = fixture.componentRef.injector.get(FirebaseProvider);
+    });
 
-  it('should be created', () => {
-    expect(component instanceof LoginFacebookComponent).toBe(true);
-  });
+    afterEach(() => {
+        fixture.destroy();
+        component = null;
+        session = null;
+        sessionSpy = null;
+        firebase = null;
+        firebaseSpy = null;
+    });
 
- });
+    it('should be created', () => {
+        expect(component instanceof LoginFacebookComponent).toBe(true);
+    });
+
+    it('should ask Firebase to authenticate via facebook', () => {
+        spyOn(component, 'viaCordova');        
+        component.loginFacebook();
+        fixture.detectChanges();
+        expect(component.viaCordova).toHaveBeenCalled();
+    });
+
+    it('should authenticate via cordova if platform is not browser', () => {
+      spyOn(component, 'cordova');
+      component.viaCordova(true);
+      fixture.detectChanges();
+      expect(component.cordova).toHaveBeenCalled();
+    });
+
+    it('should authenticate via browser if browser', () => {
+      spyOn(component, 'browser');
+      component.viaCordova(false);
+      fixture.detectChanges();
+      expect(component.browser).toHaveBeenCalled();
+    });
+
+    it('should welcome user', () => {
+        spyOn(session, 'start');
+        let user = {
+            "loggedIn": true,
+            "editor": false,
+            "uid": "test"
+          }
+        component.welcome(user);
+        fixture.detectChanges();
+        expect(session.start).toHaveBeenCalled();
+    });
+
+    it('should welcome editor user', () => {
+        spyOn(session, 'startEditor');
+        let user = {
+            "loggedIn": true,
+            "editor": true,
+            "uid": "test"
+          }
+        component.welcomeEditor(user);
+        fixture.detectChanges();
+        expect(session.startEditor).toHaveBeenCalled();
+    });
+
+});
--- src/components/login-facebook/login-facebook.ts
+++ src/components/login-facebook/login-facebook.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController, Platform } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+import * as firebase from 'firebase/app';
+
+import { HomePage } from '../../pages/home/home';
+
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import { SessionProvider } from '../../providers/session/session';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/first';
 
 @Component({
   selector: 'login-facebook',
@@ -6,7 +21,70 @@ import { Component } from '@angular/core';
 })
 export class LoginFacebookComponent {
 
-  constructor() {
+  error: any;
+
+  constructor(
+    public firebase: FirebaseProvider,
+    public session: SessionProvider,
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public platform: Platform,
+    public facebook: Facebook
+  ) {
+  }
+
+  loginFacebook() {
+    this.viaCordova(this.platform.is('cordova'))
+  }
+
+  viaCordova(cordova) {
+    if (cordova) {
+      this.cordova();
+    } else {
+      this.browser();
+    }
+  }
+
+  cordova() {
+    this.facebook.login(['email', 'public_profile']).then((token) => {
+      this.welcome(token);
+    });
+  }
+
+  browser() {
+    this.firebase.afa.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then((token)=> 
+    {
+      this.welcome(token);      
+    })
+  }
+
+  auth(token) {
+
+  }
+  
+  welcome(user) {
+    this.session.start(user);
+    this.setRootHomePage();
+  }
+
+  welcomeEditor(user) {
+    this.session.start(user);
+    this.session.startEditor();
+    this.setRootHomePage();
+  }
+
+  setRootHomePage() {
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  errorHandler(error) {
+    this.error = error;
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: error.message,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
