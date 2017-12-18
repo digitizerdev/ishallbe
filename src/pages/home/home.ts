96812ef5bb10d07e9156386fcdcc0b86656b2eb6
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-12-18 18:39:30 -0500

    feat - home storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { ProfilePage } from '../profile/profile';
 import { PostPage } from '../post/post';
