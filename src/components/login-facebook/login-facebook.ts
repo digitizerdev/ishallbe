f39c1a13d438d2fa0821ad9606b7f617c5e14413
Troy D.C. Thompson <troydcthompson@gmail.com>
2017-12-26 15:49:39 -0500

    feat - login facebook 3

--- src/components/login-facebook/login-facebook.ts
+++ src/components/login-facebook/login-facebook.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { NavController, AlertController, LoadingController, Platform } from 'ionic-angular';
 import { Facebook } from '@ionic-native/facebook';
 import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs/Observable';
+import firebase from 'firebase';
 
 import { HomePage } from '../../pages/home/home';
 
-import * as Firebase from 'firebase';
-let firebase = Firebase;
 
 import { FirebaseProvider } from '../../providers/firebase/firebase';
 
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'login-facebook',
@@ -56,19 +55,15 @@ export class LoginFacebookComponent {
     console.log("Logging in with facebook cordova");
     this.facebook.login(['email', 'public_profile']).then((token) => {
       console.log("Got token");
-      console.log(token);
-        this.facebook.getAccessToken().then((accessToken) => {
-          console.log("Facebook access token is "); 
-          console.log(accessToken);
-          let facebookProviderCredential = firebase.auth.FacebookAuthProvider.credential(accessToken);
-          console.log("Facebook provider credential is");
-          console.log(facebookProviderCredential);
-          firebase.auth().signInWithCredential(facebookProviderCredential).then((finalToken) => {
-            console.log("Final token is");
-            console.log(finalToken);
-          });         
-        })
-    });
+      console.log(token.authResponse.accessToken)
+      let facebookProviderCredential = firebase.auth.FacebookAuthProvider.credential(token);
+      console.log("Facebook provider credential is");
+      console.log(facebookProviderCredential);
+      firebase.auth().signInWithCredential(facebookProviderCredential).then((finalToken) => {
+        console.log("Final token is");
+        console.log(finalToken);
+      });
+    })
   }
 
   browser() {
@@ -83,7 +78,7 @@ export class LoginFacebookComponent {
   checkForExistingProfile(account) {
     console.log("Checking for existing account");
     console.log(account);
-    this.requestProfile(this.uid).subscribe((profile)=> {
+    this.requestProfile(this.uid).subscribe((profile) => {
       console.log("Got profile");
       console.log(profile);
       if (profile) {
@@ -92,7 +87,7 @@ export class LoginFacebookComponent {
       } else {
         this.presentEULA().subscribe((accepted) => {
           if (accepted) {
-        this.createProfile(account);
+            this.createProfile(account);
           }
         });
       }
@@ -105,29 +100,29 @@ export class LoginFacebookComponent {
   }
 
   presentEULA() {
-    return Observable.create((observer:any) => {                  
-    let alert = this.alertCtrl.create({
-      title: 'Accept Terms of Service',
-      message: 'Please confirm to continue',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-            observer.next(false);
+    return Observable.create((observer: any) => {
+      let alert = this.alertCtrl.create({
+        title: 'Accept Terms of Service',
+        message: 'Please confirm to continue',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+              observer.next(false);
+            }
+          },
+          {
+            text: 'Confirm',
+            handler: () => {
+              console.log('Confirm clicked');
+              observer.next(true);
+            }
           }
-        },
-        {
-          text: 'Confirm',
-          handler: () => {
-            console.log('Confirm clicked');
-            observer.next(true);
-          }
-        }
-      ]
-    });
-    alert.present();
+        ]
+      });
+      alert.present();
     });
   }
 
@@ -141,11 +136,11 @@ export class LoginFacebookComponent {
       "role": "contributor",
       "blocked": false
     }
-    this.firebase.object(path).set(profile).then(()=>{
+    this.firebase.object(path).set(profile).then(() => {
       this.welcome();
     })
   }
-  
+
   welcome() {
     console.log("Welcoming user");
     this.endLoader();
