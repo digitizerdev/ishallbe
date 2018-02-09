0e7ab85a531489476b5494acdd30080441149aa4
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-02-09 18:11:20 -0500

    fix - startup

--- src/pages/startup/startup.ts
+++ src/pages/startup/startup.ts
@@ -47,4 +47,62 @@ export class StartupPage {
     this.session = true;
     this.navCtrl.setRoot(HomePage);
   }
+
+ async checkChannel() {
+   console.log("Checking for channel");
+    try {
+      const res = await Pro.deploy.info();
+      this.deployChannel = res.channel;
+      this.isStaging = (this.deployChannel === 'Staging');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async toggleStaging() {
+    const config = {
+      channel: (this.isStaging ? 'Staging' : 'Master')
+    }
+      console.log("Channel is " + config.channel);
+    try {
+      await Pro.deploy.init(config);
+      await this.checkChannel();
+      await this.performAutomaticUpdate();
+    } catch (err) {
+      console.error(err);
+    }
+
+  }
+
+  async performAutomaticUpdate() {
+    console.log("Performing automatic update");
+    try {
+      const resp = await Pro.deploy.checkAndApply(true, function(progress){
+          this.downloadProgress = progress;
+      });
+      if (resp.update){
+        console.log("UPDATE AVAILABLE");
+      }else{
+        console.log("NO UPDATE AVAILABLE");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async performManualUpdate() {
+    try {
+      const haveUpdate = await Pro.deploy.check();
+      if (haveUpdate){
+        this.downloadProgress = 0;
+        await Pro.deploy.download((progress) => {
+          this.downloadProgress = progress;
+        })
+        await Pro.deploy.extract();
+        await Pro.deploy.redirect();
+      }
+    } catch (err) {
+    }
+  }
+
 }
