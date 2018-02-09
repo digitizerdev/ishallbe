2ae3a7e2d0e23065d396efae2fb1563c531caab9
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-02-09 18:13:58 -0500

    fix - startup 1

--- src/pages/startup/startup.ts
+++ src/pages/startup/startup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
+import { Pro } from '@ionic/pro';
 
 import { HomePage } from '../home/home';
 import { LoginPage } from '../login/login';
