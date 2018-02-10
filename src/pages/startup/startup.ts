7f2070eef4ceaa62e0410d1760f9369ddb9904f1
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-02-10 01:04:54 -0500

    fix - angular 5

--- src/pages/startup/startup.ts
+++ src/pages/startup/startup.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
-import { Pro } from '@ionic/pro';
 
 import { HomePage } from '../home/home';
 import { LoginPage } from '../login/login';
@@ -47,63 +46,6 @@ export class StartupPage {
   inSession() {
     this.session = true;
     this.navCtrl.setRoot(HomePage);
-  }
-
- async checkChannel() {
-   console.log("Checking for channel");
-    try {
-      const res = await Pro.deploy.info();
-      this.deployChannel = res.channel;
-      this.isStaging = (this.deployChannel === 'Staging');
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async toggleStaging() {
-    const config = {
-      channel: (this.isStaging ? 'Staging' : 'Master')
-    }
-      console.log("Channel is " + config.channel);
-    try {
-      await Pro.deploy.init(config);
-      await this.checkChannel();
-      await this.performAutomaticUpdate();
-    } catch (err) {
-      console.error(err);
-    }
-
-  }
-
-  async performAutomaticUpdate() {
-    console.log("Performing automatic update");
-    try {
-      const resp = await Pro.deploy.checkAndApply(true, function(progress){
-          this.downloadProgress = progress;
-      });
-      if (resp.update){
-        console.log("UPDATE AVAILABLE");
-      }else{
-        console.log("NO UPDATE AVAILABLE");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  async performManualUpdate() {
-    try {
-      const haveUpdate = await Pro.deploy.check();
-      if (haveUpdate){
-        this.downloadProgress = 0;
-        await Pro.deploy.download((progress) => {
-          this.downloadProgress = progress;
-        })
-        await Pro.deploy.extract();
-        await Pro.deploy.redirect();
-      }
-    } catch (err) {
-    }
   }
 
 }
