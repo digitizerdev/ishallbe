08f8196c9f1941b78836475d2539fc02db3b833f
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-03-19 01:11:45 -0400

    test firebase send message

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,36 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    console.log("Hello World Triggered");
+    response.send("Hello World from Firebase!");
 });
+
+exports.createPin = functions.firestore.document('pins/{pinId}').onCreate(event => {
+    console.log("Create Pin Triggered");
+    var newValue = event.data.data();
+    console.log(newValue);
+    var message = "New Pin Added : " + newValue.title;
+    pushMessage(message);
+    return true;
+});
+
+function pushMessage(message) {
+    console.log("Push Message Triggered");
+    console.log("Message is " + message);
+    var payload = {
+        notification: {
+            title: message,
+        }
+    };
+    admin.messaging().sendToTopic("notifications", payload)
+        .then(function (response) {
+            console.log("Successfully sent message:", response);
+            return response;
+        })
+        .catch(function (error) {
+            console.log("Error sending message:", error);
+            return error;
+        });
+}
