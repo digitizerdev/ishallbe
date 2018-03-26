62d54a304ddb5eb7821ef1bfce02b4ccc2c7b5cd
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-03-26 17:41:58 -0400

    fix pin liked

--- src/components/pin/pin.ts
+++ src/components/pin/pin.ts
@@ -1,28 +1,62 @@
 import { Component, Input } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 import { NavController } from 'ionic-angular';
 
 import { PostPage } from '../../pages/post/post';
+
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import { AngularFirestoreDocument } from 'angularfire2/firestore';
+
+import { Pin } from '../../../test-data/pins/model';
 
 @Component({
   selector: 'pin',
   templateUrl: 'pin.html'
 })
 export class PinComponent {
-  @Input('post') pin;
+  @Input('post') post;
+  private pinDoc: AngularFirestoreDocument<Pin>;
+  pin: any;
+  loaded = false;
 
   constructor(
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private firebase: FirebaseProvider
   ) { }
 
   ngAfterViewInit() {
-    console.log("Pin initialized");
-    console.log(this.pin);
+    this.loadPin();
+  }
+
+  loadPin() {
+    let pinPath = "pins/" + this.post.id;
+    this.pinDoc = this.firebase.afs.doc<Pin>(pinPath);
+    this.pinDoc.valueChanges().subscribe((pin) => {
+      this.pinLiked(pin).subscribe((pin) => {
+        this.pin = pin;
+        this.loaded = true;
+      });
+    })
+  }
+
+  pinLiked(pin) {
+    pin.liked = false;
+    return Observable.create((observer) => {
+      let pinLikePath = "pins/" + pin.id + "/likes/" + this.firebase.user.uid;
+      this.firebase.afs.doc(pinLikePath).valueChanges().subscribe((like) => {
+        if (!this.loaded) {
+          if (like) pin.liked = true;
+          observer.next(pin);
+        }
+      });
+    });
   }
 
   viewPin() {
     this.navCtrl.push(PostPage, { 
-      id: this.pin.id,
+      id: this.post.id,
       type: "pin"
      });
   }
