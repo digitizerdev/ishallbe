9c0046de30b835814c2da4a7f390fb01aa2e30b3
Troy D.C. Thompson <troydcthompson@gmail.com>
2018-05-28 19:11:12 -0400

    api manager analytics

--- src/pages/api-manager/api-manager.ts
+++ src/pages/api-manager/api-manager.ts
@@ -5,7 +5,14 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 import { HomePage } from '../home/home';
 
+import { AngularFirestoreCollection } from 'angularfire2/firestore';
 import { FirebaseProvider } from '../../providers/firebase/firebase';
+
+import { User } from '../../../test-data/users/model';
+import { Pin } from '../../../test-data/pins/model';
+import { Statement } from '../../../test-data/statements/model';
+import { Goal } from '../../../test-data/goals/model';
+
 
 @IonicPage()
 @Component({
@@ -14,10 +21,14 @@ import { FirebaseProvider } from '../../providers/firebase/firebase';
 })
 export class ApiManagerPage {
 
-  user: any;
-  deployChannel = "";
-  isBeta = false;
-  editor = false;
+  userCollection: AngularFirestoreCollection<User>;
+  pinCollection: AngularFirestoreCollection<Pin>;
+  statementCollection: AngularFirestoreCollection<Statement>;
+  goalCollection: AngularFirestoreCollection<Goal>;
+  users = [];
+  pins = [];
+  statements = [];
+  goals = [];
 
   constructor(
     private navCtrl: NavController,
@@ -27,16 +38,47 @@ export class ApiManagerPage {
   }
 
   ionViewDidLoad() {
-    this.user = this.firebase.user;
+    this.loadUsers();
+    this.loadPins();
+    this.loadStatements();
+    this.loadGoals();  }
+
+  loadUsers() {
+    console.log("Loading Users");
+    this.userCollection = this.firebase.afs.collection('users');
+    this.userCollection.valueChanges().subscribe((users) => {
+      console.log("Users length is " + users.length);
+      this.users = users;
+    });
   }
 
+  loadPins() {
+    console.log("Loading Pins");
+    this.pinCollection = this.firebase.afs.collection('pins');
+    this.pinCollection.valueChanges().subscribe((pins) => {
+      console.log("Pins length is " + pins.length);
+      this.pins = pins;
+    });
+  }
+
+  loadStatements() {
+    console.log("Loading Statements");
+    this.statementCollection = this.firebase.afs.collection('statements');
+    this.statementCollection.valueChanges().subscribe((statements) => {
+      console.log("Statements length is " + statements.length);
+      this.statements = statements;
+    });
+  }
+
+  loadGoals() {
+    console.log("Loading Goals");
+    this.goalCollection = this.firebase.afs.collection('goals');
+    this.goalCollection.valueChanges().subscribe((goals) => {
+      console.log("Goals length is " + goals.length);
+      this.goals = goals;
+    });
+  }
   openLink() {
     this.iab.create('https://tdct.io', '_system');
   }
-
-  setRootHomePage() {
-    this.navCtrl.setRoot(HomePage);
-  }
-
-
 }
